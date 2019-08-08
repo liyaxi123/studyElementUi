3eae535e4927b9d3d2fe6528a4cd3db71c1e61f3
liyaxi <1148387889@qq.com>
2019-08-08 08:50:58 +0800

    级联面板研究2

--- src/test.js
+++ src/test.js
@@ -28,3 +28,57 @@ const focusNode = el => {
   el.focus()
   !isLeaf(el) && el.click()
 }
+
+const checkNode = el => {
+  if (!el) return
+  const input = el.querySelector('input')
+  if (input) {
+    input.click()
+  } else if (isLeaf(el)) {
+    el.click()
+  }
+}
+function noop () {}
+const DefaultProps = {
+  expandTrigger: 'click',
+  multiple: false,
+  checkStrictly: false,
+  emitPath: true,
+  lazy: false,
+  value: 'value',
+  label: 'label',
+  children: 'children',
+  leaf: 'leaf',
+  disable: 'disable',
+  hoverThreshold: 500,
+  lazyLoad: noop
+ }
+ const props = {
+   value: {},
+   options:Arrray,
+   props: Object,
+   border: {
+     type: Boolean,
+     default: true
+   },
+   renderLabel: Function
+ }
+function merge (target) {
+  // 从1开始循环
+  for (let i = 1, j = arguments.length; i < j; i++){
+    let source = arguments[i] || {}
+    for (let prop in source) {
+      if (source.hasOwnProperty(prop)) {
+        let value = source[prop]
+        if (value !== undefined) {
+          target[prop] = value
+        }
+      }
+    }
+  }
+  return target
+}
+
+function renderLabelFn() {
+  return this.renderLabel || this.$scopedSlots.default
+}
